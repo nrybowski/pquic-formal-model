e2572ae0657b03b537a028615c377ca99a103a95
nrybowski <nicolas.rybowski@student.uclouvain.be>
2021-02-10 23:14:24 +0100

    Add 'main' annotation generator

---
+++ checks/specs_check__update_ack_delay.c
@@ -0,0 +1,37 @@
+#include "verifier/verifier.h"
+
+protoop_arg_t update_ack_delay(picoquic_cnx_t *cnx);
+int main()
+{
+  picoquic_cnx_t cnx;
+  picoquic_cnx_t cnx0;
+  init__picoquic_cnx_t(&cnx);
+  assume_cp__picoquic_cnx_t(&cnx, &cnx0);
+  assume(cnx.protoop_inputc == 4);
+  picoquic_packet_context_t pkt_ctx;
+  picoquic_packet_context_t pkt_ctx0;
+  init__picoquic_packet_context_t(&pkt_ctx);
+  assume_cp__picoquic_packet_context_t(&pkt_ctx, &pkt_ctx0);
+  cnx.protoop_inputv[0] = (protoop_arg_t) (&pkt_ctx);
+  picoquic_path_t old_path;
+  picoquic_path_t old_path0;
+  init__picoquic_path_t(&old_path);
+  assume_cp__picoquic_path_t(&old_path, &old_path0);
+  cnx.protoop_inputv[1] = (protoop_arg_t) (&old_path);
+  int64_t rtt_estimate;
+  rtt_estimate = dummy__int64_t();
+  cnx.protoop_inputv[2] = rtt_estimate;
+  bool first_estimate;
+  first_estimate = dummy__bool();
+  cnx.protoop_inputv[3] = first_estimate;
+  update_ack_delay(&cnx);
+  sassert((&pkt_ctx) == ((picoquic_packet_context_t *) cnx.protoop_inputv[0]));
+  assert_cp__picoquic_packet_context_t((picoquic_packet_context_t *) cnx.protoop_inputv[0], &pkt_ctx0, ASSERT_PICOQUIC_PACKET_CONTEXT_T__ACK_DELAY_LOCAL);
+  sassert((&old_path) == ((picoquic_path_t *) cnx.protoop_inputv[1]));
+  assert_cp__picoquic_path_t((picoquic_path_t *) cnx.protoop_inputv[1], &old_path0, ASSERT_NONE);
+  sassert(rtt_estimate == ((int64_t) cnx.protoop_inputv[2]));
+  sassert(first_estimate == ((bool) cnx.protoop_inputv[3]));
+  assert_cp__picoquic_cnx_t(&cnx, &cnx0, ASSERT_NONE);
+  return 0;
+}
+
---
+++ generate_annotated_main.py
@@ -0,0 +1,112 @@
+from json import load
+from sys import argv
+
+from pycparser import c_parser, c_ast, c_generator
+
+def usage():
+    print('usage')
+
+def generate_main():
+    parser = c_parser.CParser()
+    fun_ast = parser.parse('')
+
+    body = []
+
+    for cnx in ['cnx', 'cnx0']:
+        # Add : picoquic_cnx_t <cnx>;
+        body.append(c_ast.Decl(cnx, [], [], [], c_ast.TypeDecl(cnx, [], c_ast.IdentifierType(['picoquic_cnx_t'])), None, None))
+
+    # Add : init__picoquic_cnx_t(&cnx);
+    body.append(c_ast.FuncCall(c_ast.ID('init__picoquic_cnx_t'), c_ast.ExprList([c_ast.ID('&cnx')])))
+
+    # Add : assume_cp__picoquic_cnx_t(&cnx, &cnx0);
+    body.append(c_ast.FuncCall(c_ast.ID('assume_cp__picoquic_cnx_t'), c_ast.ExprList([c_ast.ID('&cnx'), c_ast.ID('&cnx0')])))
+
+    # Add : assume(cnx.protoop_inputc == 3);
+    body.append(c_ast.FuncCall(c_ast.ID('assume'), c_ast.ExprList([c_ast.BinaryOp('==', c_ast.StructRef(c_ast.ID('cnx'), '.', c_ast.ID('protoop_inputc')), c_ast.Constant('int', str(len(protoop_specs['inputs']))))])))
+
+    idx = 0
+    for input_name, input_type in protoop_specs['inputs'].items():
+        # Add : <type> <name>;
+        body.append(c_ast.Decl(input_name, [], [], [], c_ast.TypeDecl(input_name, [], c_ast.IdentifierType([input_type[0]])), None, None))
+        left = c_ast.ArrayRef(c_ast.StructRef(c_ast.ID('cnx'), '.', c_ast.ID('protoop_inputv')), c_ast.Constant('int', str(idx)))
+        if '*' in input_type:
+            # Add : <type> <name>0;
+            body.append(c_ast.Decl('%s0' % input_name, [], [], [], c_ast.TypeDecl('%s0' % input_name, [], c_ast.IdentifierType([input_type[0]])), None, None))
+            # Add : init__<type>(&<name>);
+            body.append(c_ast.FuncCall(c_ast.ID('init__%s' % input_type[0]), c_ast.ExprList([c_ast.UnaryOp('&', c_ast.ID(input_name))])))
+            body.append(c_ast.FuncCall(c_ast.ID('assume_cp__%s' % input_type[0]), c_ast.ExprList([c_ast.ID('&%s' % input_name), c_ast.UnaryOp('&', c_ast.ID('%s0' % input_name))])))
+            right = c_ast.Cast(c_ast.Typename(None, [], c_ast.TypeDecl(None, [], c_ast.IdentifierType(['protoop_arg_t']))), c_ast.UnaryOp('&', c_ast.ID(input_name)))
+        else:
+            body.append(c_ast.Assignment('=', c_ast.ID(input_name), c_ast.FuncCall(c_ast.ID('dummy__%s' % input_type[0]), c_ast.ExprList([]))))
+            right = c_ast.ID(input_name)
+
+        body.append(c_ast.Assignment('=', left, right, None))
+
+        idx += 1
+
+    # Add : <plugin>(cnx);
+    body.append(c_ast.FuncCall(c_ast.ID(protoop_specs['name']), c_ast.ExprList([c_ast.ID('&cnx')])))
+
+    idx = 0
+    for input_name, input_type in protoop_specs['inputs'].items():
+        right_ref = c_ast.ArrayRef(c_ast.StructRef(c_ast.ID('cnx'), '.', c_ast.ID('protoop_inputv')), c_ast.Constant('int', str(idx)))
+        right = c_ast.Cast(c_ast.Typename(None, [], c_ast.TypeDecl(None, [], c_ast.IdentifierType([input_type[0]]))), right_ref)
+        if '*' in input_type:
+            if input_name not in protoop_specs['effects']:
+                flag = 'ASSERT_NONE' 
+            else:
+                flag = ' | '.join([str.upper('assert_%s__%s' % (input_type[0], field)) for field in  protoop_specs['effects'][input_name]])
+            
+            cnx_input = c_ast.Cast(c_ast.Typename(None, [], c_ast.PtrDecl([], c_ast.TypeDecl(None, [], c_ast.IdentifierType([input_type[0]])))), right_ref)
+            
+            body.append(c_ast.FuncCall(c_ast.ID('sassert'), c_ast.ExprList([c_ast.BinaryOp('==', c_ast.UnaryOp('&', c_ast.ID(input_name)), cnx_input)])))
+
+            body.append(c_ast.FuncCall(c_ast.ID('assert_cp__%s' % input_type[0]), 
+                c_ast.ExprList([
+                    cnx_input,
+                    c_ast.UnaryOp('&', c_ast.ID('%s0'%input_name)),
+                    c_ast.ID(flag)
+                ])))
+        else:
+            body.append(c_ast.FuncCall(c_ast.ID('sassert'), c_ast.ExprList([c_ast.BinaryOp('==', c_ast.ID(input_name), right)])))
+        idx += 1
+
+
+
+    # Add : assert_cp__picoquic_cnx_t(&cnx, &cnx0, ASSERT_NONE);
+    body.append(c_ast.FuncCall(c_ast.ID('assert_cp__picoquic_cnx_t'), c_ast.ExprList([c_ast.ID('&cnx'), c_ast.ID('&cnx0'), c_ast.ID('ASSERT_NONE')])))
+
+    # Add : return 0;
+    body.append(c_ast.Return(c_ast.Constant('int', '0')))
+    
+    protoop_params = [c_ast.Decl('cnx', [], [], [], c_ast.PtrDecl([], c_ast.TypeDecl('cnx', [], c_ast.IdentifierType(['picoquic_cnx_t']))), None, None)] 
+    protoop_decl = c_ast.FuncDecl(c_ast.ParamList(protoop_params), c_ast.TypeDecl(protoop_specs['name'], [], c_ast.IdentifierType(['protoop_arg_t'])))
+    protoop_signature = c_ast.Decl(protoop_specs['name'], [], [], [], protoop_decl, None, None)
+    fun_ast.ext.append(protoop_signature)
+
+    main_decl = c_ast.FuncDecl(c_ast.ParamList([]), c_ast.TypeDecl('main', [], c_ast.IdentifierType(['int'])))
+    main_signature = c_ast.Decl('main', [], [], [], main_decl, None, None)
+    fun_ast.ext.append(c_ast.FuncDef(main_signature, [], c_ast.Compound(body)))
+
+    return fun_ast
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        usage()
+        exit(1)
+
+    # TODO : check that argv1 is a reachable file
+    fn = argv[1]
+
+    with open(fn, 'r') as fp:
+        protoop_specs = load(fp)
+
+    main_ast = generate_main()
+    generator = c_generator.CGenerator()
+
+    with open('checks/specs_check__%s.c' % protoop_specs['name'], 'w') as output:
+        output.write('#include "verifier/verifier.h"\n\n')
+        output.write(generator.visit(main_ast))
+
