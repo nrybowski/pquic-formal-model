28c7bf42f10a2e8915f59977b497bb430adbdb6f
nrybowski <nicolas.rybowski@student.uclouvain.be>
2021-02-25 02:29:03 +0100

    Fix missing assume in buffer2.c and Add esbmc test

--- example/buffer_esbmc.c
+++ example/buffer_esbmc.c
@@ -1,8 +1,7 @@
-#include "seahorn/seahorn.h"
 #include <stdlib.h>
 
-extern unsigned int nd();
-extern char nd2();
+//extern unsigned int nd();
+//extern char nd2();
 
 typedef struct st_my_struct {
         unsigned int length;
@@ -10,11 +9,11 @@ typedef struct st_my_struct {
 } my_struct;
 
 inline void init(my_struct * s) {
-        s->length = nd();
+        s->length = nondet_uint();
         s->buf = malloc(sizeof(char) * s->length);
-        assume(s->buf != NULL);
+        __ESBMC_assume(s->buf != NULL);
         for (int i=0; i<s->length; i++)
-                *(s->buf+i) = nd2();
+                *(s->buf+i) = nondet_char();
 }
 
 inline void clean(my_struct * s) {
@@ -24,15 +23,15 @@ inline void clean(my_struct * s) {
 inline void duplicate(my_struct *src, my_struct *dst) {
         dst->length = src->length;
         dst->buf = malloc(sizeof(char) * dst->length);
-        assume(dst->buf != NULL);
+        __ESBMC_assume(dst->buf != NULL);
         for (int i=0; i<src->length; i++)
                 *(dst->buf+i) = *(src->buf+i);
 }
 
 inline void check(my_struct *src, my_struct *dst) {
-        sassert(dst->length == src->length);
+        __ESBMC_assert(dst->length == src->length, "");
         for (int i=0; i<src->length; i++)
-                sassert(*(dst->buf+i) == *(src->buf+i));
+                __ESBMC_assert(*(dst->buf+i) == *(src->buf+i), "");
 }
 
 int main() {
@@ -43,17 +42,17 @@ int main() {
         check(&s, &s0); // should unsat
 
         // choose a position in the buffer to change
-        unsigned int b = nd();
-        assume(b<s.length);
+        unsigned int b = nondet_uint();
+        __ESBMC_assume(b<s.length);
 
         // choose a byte different from the one currently at the position
-        char x = nd2();
-        assume(x != *(s.buf+b));
+        char x = nondet_char();
+        __ESBMC_assume(x != *(s.buf+b));
 
         // set byte
         *(s.buf+b) = x;
 
-        check(&s, &s0); // should sat
+        //check(&s, &s0); // should sat
 
         clean(&s);
         clean(&s0);
