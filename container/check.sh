725f48b051adaaab890ac8d1fb6dcb6eea96b3e8
nrybowski <nicolas.rybowski@student.uclouvain.be>
2021-03-08 16:31:41 +0100

    Set configurable BMC in custom SH container

--- container/check.sh
+++ container/check.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-sea bpf -g --bmc=opsem --inline --crab --track=mem --dsa=sea-cs -m64 -DDISABLE_PROTOOP_PRINTF -DDISABLE_QLOG -Ipquic/picoquic pquic/picoquic/getset.c pquic-formal-model/verifier/verifier.c pquic/plugins/${1}/${2}.c pquic-formal-model/checks/specs_check__${2}.c
+sea bpf -g --bmc="${BMC}" --inline --crab --track=mem --dsa=sea-cs -m64 -DDISABLE_PROTOOP_PRINTF -DDISABLE_QLOG -Ipquic/picoquic pquic/picoquic/getset.c pquic-formal-model/verifier/verifier.c pquic/plugins/${1}/${2}.c pquic-formal-model/checks/specs_check__${2}.c
