782ce75ba065b2294c21297b9ae1f7127798b873
nrybowski <nicolas.rybowski@student.uclouvain.be>
2021-06-14 07:29:07 +0200

    Update simple_wrapper

--- simple_wrapper.sh
+++ simple_wrapper.sh
@@ -7,7 +7,7 @@ CONTAINERNAME="seahorn_base"
 cat << EOF >> ${DOCKERFILE}
 FROM seahorn/seahorn-llvm10:nightly
 RUN echo horn | sudo -S apt-get update
-RUN echo horn | sudo -S DEBIAN_FRONTEND=noninteractive su -p - -c "apt-get install -y --no-install-recommends --fix-missing libarchive-dev libssl-dev"
+RUN echo horn | sudo -S DEBIAN_FRONTEND=noninteractive su -p - -c "apt-get install -y --no-install-recommends --fix-missing libarchive-dev libssl-dev valgrind gdb time"
 EOF
 
 docker build -t "${CONTAINERNAME}" -f "${DOCKERFILE}" .
@@ -15,12 +15,14 @@ rm "${DOCKERFILE}"
 
 BPF_OPTS="-m64 -g --bmc=mono --inline --track=mem --dsa=sea-cs \
 	--cex=witness.ll --log=cex --bv-cex --horn-bv-part-mem \
-	-I/opt/picoquic -DDISABLE_PROOTOP_PRINTF -DDISABLE_QLOG /opt/model/verifier/verifier.c /opt/picoquic/*.c"
+	-I/opt/picoquic -DDISABLE_PROTOOP_PRINTF -DDISABLE_QLOG /opt/model/verifier/verifier.c /opt/picoquic/*.c"
 
 EXE_OPTS="-g -m64 --alloc-mem -o out witness.ll"
 
+#	--cap-add=SYS_ADMIN \
 docker run \
 	--rm -it \
+	--tmpfs /tmp \
 	-v "${PWD}/verifier:/opt/model/verifier" \
 	-v $(pwd)/checks:/opt/model/checks \
 	-v $(pwd)/pquic/picoquic:/opt/picoquic \
